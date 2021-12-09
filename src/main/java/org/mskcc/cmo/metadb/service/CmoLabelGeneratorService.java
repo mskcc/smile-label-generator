5dcb44c31faed1a0e6b4a8cc57c986da110386f4
Angelica Ochoa <15623749+ao508@users.noreply.github.com>
2021-12-09 15:13:00 -0500

    Updates to MetaDB model dependency and related changes
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>

--- src/main/java/org/mskcc/cmo/metadb/service/CmoLabelGeneratorService.java
+++ src/main/java/org/mskcc/cmo/metadb/service/CmoLabelGeneratorService.java
@@ -2,11 +2,13 @@ package org.mskcc.cmo.metadb.service;
 
 import java.util.List;
 import org.mskcc.cmo.metadb.model.SampleMetadata;
+import org.mskcc.cmo.metadb.model.igo.IgoSampleManifest;
 
 /**
  *
  * @author ochoaa
  */
 public interface CmoLabelGeneratorService {
-    String generateCmoSampleLabel(SampleMetadata sampleMetadata, List<SampleMetadata> existingPatientSamples);
+    String generateCmoSampleLabel(String requestId,
+            IgoSampleManifest sampleManifest, List<SampleMetadata> existingPatientSamples);
 }
